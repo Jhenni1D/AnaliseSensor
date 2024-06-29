5e4b7d3ab09b9854d5a54253f952a61746875123
Elielson Barbosa <elielsonbr.com@gmail.com>
2024-06-29 09:55:43 -0300

    feat: ajustando imagens que serão plotadas e progresso

--- server/static/js/socket.js
+++ server/static/js/socket.js
@@ -1,64 +1,69 @@
-$(document).ready(function(){
-  let m_container = document.querySelector("#m_container");
-  let t_container = document.querySelector("#t_container");
-  let termico_container = document.querySelector("#termico");
+$(document).ready(function () {
   let progresso = document.querySelector("#progresso");
   let namespace = '';
-  let host = location.protocol + '//' + document.domain + ':' + location.port + namespace
+  let host = location.protocol + '//' + document.domain + ':' + location.port + namespace;
+
+  let eficience_percent = document.querySelector("#eficiencia");
+
+  let m_images = {
+    "M0": document.querySelector("#m0"),
+    "M1": document.querySelector("#m1"),
+    "M2": document.querySelector("#m2"),
+    "M3": document.querySelector("#m3")
+  }
+
+  let t_images = {
+    "T0": document.querySelector("#t0"),
+    "T1": document.querySelector("#t1"),
+    "T2": document.querySelector("#t2"),
+    "T3": document.querySelector("#t3")
+  }
+
+  let termico_image = document.querySelector("#termico")
+
   console.log(dado)
   console.log(host)
-  if(dado.length !== 0)
-  {
-    for(d of dado)
-      {
-        check_data_type_and_create(d);
+
+  if (dado.length !== 0) {
+    for (d of dado) {
+      if (d["name"] in m_images) {
+        m_images[d["name"]].src = `/static/image_test/${d["name"]}.png`;
       }
+
+      // if (d["name"] in t_images) {
+      //   m_images[d["name"]].src = img_source.replace("img_name", d["img"])
+      // }
+
+      // if (d["name"] == "TERMICO") {
+      //   termico_image.src = img_source.replace("img_name", d["img"])
+      // }
+    }
   }
+
+  /*
+      LÓGICA DO SOCKETIO ABAIXO.
+  */
+
   let socket = io();
 
-  socket.on('connect', function() {
+  socket.on('connect', function () {
     console.log('Connected!');
-
+    socket.emit('progress_test');
   });
 
-  function create_img(name, img_src)
-  {
-    let h3 = document.createElement('h3');
-    h3.innerHTML = name
-	  let img = document.createElement('img');
-    img.src = img_src
-    let div = document.createElement('div');
-    div.append(h3);
-    div.append(img)
-    return div
-  }
 
-  function check_data_type_and_create(data){
-    if(data["type"] === "M")
-    {
-      m_container.append(create_img(data["name"], data["img"]))
-    }
-    else if(data["type"] === "T")
-    {
-      t_container.append(create_img(data["name"], data["img"]))
-    }
-    else
-    {
-                termico_container.append(create_img(data["name"], data["img"]))
-    }
-  }
-  
-  socket.on('plot_image', function(image_data) {
-    check_data_type_and_create(image_data)
+  socket.on('plot_image', function (image_data) {
+    // TODO: inserir lógica de por imagens aqui
   });
 
-  
 
-  socket.on('progress_value', function(prog_value) {
-    progresso.innerHTML = "Progresso: "+prog_value+"%"
+
+  socket.on('progress_value', function (prog_value) {
+    progresso.style["width"] = `${prog_value}%`;
+    progresso.innerHTML = `${prog_value}%`;
   });
 
-  socket.on('disconnect', function() {
+  socket.on('disconnect', function () {
     console.log('Disconnected!');
   });
 });
