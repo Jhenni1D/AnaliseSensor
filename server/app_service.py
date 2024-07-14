cf06aa940736f9b8a2fce15fd5e045b7f7ce0804
Elielson Barbosa <elielsonbr.com@gmail.com>
2024-07-14 14:32:46 -0300

    feat: add funcionalidades para criar dados sem ter nome aleatório

--- server/app_service.py
+++ server/app_service.py
@@ -1,28 +1,68 @@
 #Funções serviços dos aplicativos
 import csv
 import datetime
+import time
 import json
 import os
 import shutil
 from datetime import date
-from requests import get, post
+from requests import get, post, patch
 from log_handler import write_log
 from firebase_admin import credentials, initialize_app, storage
 
 link_bd_base = "https://simulacao-femm-default-rtdb.firebaseio.com/"
 
 link_bd = link_bd_base+"/medicoes/{}/.json"
+link_bd_folders = link_bd.format("Pastas")
 link_bd_todos_sensores = link_bd_base+"/medicoes/.json"
 link_bd_image = "simulacao-femm.appspot.com"
 link_bd_image_gs = f"gs://{link_bd_image}"
 
 def pegar_data_formatada():
     data_atual = date.today()  # date é a lib
-    return "{}/{}/{}".format(data_atual.day, data_atual.month,data_atual.year)  # formatando a data contatenar dados
+    return "{}/{}/{}".format(data_atual.day, data_atual.month, data_atual.year)  # formatando a data contatenar dados
 
 def pegar_hora_formatada():
     now = datetime.datetime.now()  # agora pegar a hora
-    return str(now.hour) + ":" + str(now.minute) + ":" + str(now.second)  # concatenar o foamato da hora
+    return str(now.hour).zfill(2) + ":" + str(now.minute).zfill(2) + ":" + str(now.second).zfill(2)  # concatenar o foamato da hora
+
+def pegar_nome_pasta():
+    data_atual = date.today()
+    data_atual = "{}-{}-{}".format(str(data_atual.day).zfill(2), str(data_atual.month).zfill(2), data_atual.year)
+
+    time_now = datetime.datetime.now()  # agora pegar a hora
+    milliseconds = str(round(time.time() * 1000, 4))[-4:].replace(".", "")
+
+    time_now = str(time_now.hour).zfill(2) + "-" + str(time_now.minute).zfill(2) + "-" + str(time_now.second).zfill(2) + "-" + str(
+        milliseconds)  # concatenar o foamato da hora
+
+    nome_pasta = f'{data_atual}_{time_now}'
+
+    return nome_pasta
+
+def pegar_nova_pasta_formatada():
+    nova_pasta_name = pegar_nome_pasta()
+    nova_pasta = {
+        nova_pasta_name:
+            {
+                "Sensores":
+                {
+                    "SensorA": [],
+                    "SensorB": [],
+                    "SensorC": [],
+                    "SensorRPM": [],
+                    "SensorTemp": [],
+                    "SensorTensao": []
+                },
+                "completed": False
+            }
+    }
+
+    return nova_pasta_name, nova_pasta
+
+def register_new_folder(folder_data):
+    patch(link_bd_folders, json=folder_data)
+
 
 def escrever_dados_arquivo_csv(dados): #dados parametros aula lira
     for sensor in dados:
@@ -32,6 +72,14 @@ def escrever_dados_arquivo_csv(dados): #dados parametros aula lira
             wr.writeheader()  # titulo
             wr.writerows(dados[sensor]) # dados de cada coluna
     shutil.make_archive('output', 'zip', './', 'server/outputs')
+
+
+def get_folder_incompleted():
+    folders = get(link_bd_folders).json()
+    folder_incompleted = [(folder, {folder: folders[folder]}) for folder in folders if "completed" in folders[folder] and folders[folder]["completed"] is False]
+    return folder_incompleted
+
+
 
 def registrar_dado_no_bd(dados, sensor):
     post(link_bd.format(sensor), json=dados)
