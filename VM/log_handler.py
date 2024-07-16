a82fae0ae0e7370eae9e97afce716b89a36fd850
Elielson Barbosa <elielsonbr.com@gmail.com>
2024-07-16 19:28:46 -0300

    fix: ajusta path da criação do log

--- VM/log_handler.py
+++ VM/log_handler.py
@@ -1,6 +1,5 @@
 folder_name = ""
 
-
 def set_folder_log(folder):
     global folder_name
     folder_name = folder
@@ -12,8 +11,8 @@ def write_log(msg, folder="", create=False):
         folder_name = folder
     if create:
         folder_name=folder
-        with open(f"log-simulacao_{folder_name}.txt", "w", encoding="utf-8") as file:
+        with open(f"./log-simulacao_{folder_name}.txt", "w", encoding="utf-8") as file:
             file.write(msg+"\n")
         return
-    with open(f"log-simulacao_{folder_name}.txt", "a", encoding="utf-8") as file:
+    with open(f"./log-simulacao_{folder_name}.txt", "a", encoding="utf-8") as file:
         file.write(msg+"\n")
