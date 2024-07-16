7ea942e8ab8f9b742bfd178a5afa7d6eecfea586
Elielson Barbosa <elielsonbr.com@gmail.com>
2024-07-16 19:26:38 -0300

    feat: utiliza aplicação para executar as análises de simulação, busca os dados dos sensores B, C e temp já no momento em que vai inserir na fila e muda a forma de executar o arquivo externo

--- VM/test_socketio.py
+++ VM/test_socketio.py
@@ -7,6 +7,8 @@ import queue
 import socketio
 
 import simulation
+from app_service import pegar_ultimo_dado_do_sensor
+
 
 class LoopState:
 
@@ -16,100 +18,109 @@ class LoopState:
 
 sio = socketio.Client()
 fila = queue.Queue()
-file_simulation = os.path.abspath("simulation.py").replace("\\", "/")
+file_simulation = os.path.abspath("simulation.exe").replace("\\", "/")
+print(file_simulation)
 simulation_controller = simulation.SimulationController()
 simulation_state = LoopState()
 
-@sio.event
-def connect():
-    sio.emit("start_att_img")
-    sio.emit("start_simulation_loop")
-    print('connection established')
-    sio.emit("ping")
-    print("send ping")
+try:
 
-@sio.event
-def pong():
-    print("receive pong")
+    @sio.event
+    def connect():
+        sio.emit("start_att_img")
+        sio.emit("start_simulation_loop")
+        print('connection established')
+        sio.emit("ping")
+        print("send ping")
 
-@sio.event
-def insert_queue(data):
-    simulation_controller.load_simulation()
-    is_can_enqueue = simulation_controller.is_can_enqueue(data["medicao"])
-    if is_can_enqueue:
-        fila.put(data)
-        simulation_controller.update_queue(data["folder_name"])
-        print("Inseriu elemento na fila, elementos na fila:", fila.qsize(), " | dados:", data)
+    @sio.event
+    def pong():
+        print("receive pong")
 
+    @sio.event
+    def insert_queue(data):
+        simulation_controller.load_simulation()
+        is_can_enqueue = simulation_controller.is_can_enqueue(data["medicao"])
+        # TODO: SensorC1/ ...C2 / ...C3 são a mesma coisa que ABC.
 
-
-@sio.event
-def simulation_loop():
-    if simulation_state.simulation_running:
-        print("Loop simulação já está rodando!")
-        return
-    while True:
-        simulation_state.simulation_running = True
-        while not fila.empty():
-            data = fila.get()
-            print('Irá iniciar simulação com os dados:', data)
-            p = subprocess.run([sys.executable, file_simulation, json.dumps([data["medicao"], data["sensor"]])],
-                               capture_output=True, text=True)
-            print("LOG EXECUCAO SUBPROCESS:", p.stdout)
-    simulation_state.simulation_running = False
-
-@sio.event
-def send_img_loop():
-    if simulation_state.files_running:
-        print("Loop dos arquivos já está rodando!")
-        return
-    print("Iniciou o loop dos arquivos")
-    img_file_name = "img_send.json"
-    folder_file_name = "folder_name.txt"
-    progress_file_name = "progress_simulation.txt"
-    while True:
-        simulation_state.files_running = True
-        time.sleep(3)
-        dir_list = os.listdir("./")
-        if img_file_name in dir_list:
+        # TODO: valor da temperatura vai entrar na medição
+        if is_can_enqueue:
+            data["medicaoA"] = data["medicao"]
+            del data["medicao"]
             try:
-                with open(img_file_name) as file:
-                    sio.emit('request_update_image', json.loads(file.read()))
-                os.remove(img_file_name)
-            except:
-                print("Quebrou ao tentar abrir arquivo das imagens")
-
-        if folder_file_name in dir_list:
-            try:
-                pass
-                # with open(folder_file_name) as file:
-                #     sio.emit('criar_pasta', file.read())
-                # os.remove(folder_file_name)
-            except:
-                print("Quebrou ao tentar abrir arquivo de pasta")
-
-        if progress_file_name in dir_list:
-            try:
-                with open(progress_file_name) as file:
-                    sio.emit('progress', file.read())
-            except:
-                print("Quebrou ao tentar abrir arquivo progress")
-    print("Finalizou Loop")
-    simulation_state.files_running = False
+                data["medicaoB"] = float(pegar_ultimo_dado_do_sensor("SensorB")["medicao"])
+                data["medicaoC"] = float(pegar_ultimo_dado_do_sensor("SensorC")["medicao"])
+                data["medicaoTemp"] = float(pegar_ultimo_dado_do_sensor("SensorTemp")["medicao"])
+            except Exception as e:
+                print("Erro ao buscar valores dos sensores B, C e da Temperatura! Verifique se os valores existem no firebase!")
+                print(e)
+                return
+            fila.put(data)
+            simulation_controller.update_queue(data["folder_name"])
+            print("Inseriu elemento na fila, elementos na fila:", fila.qsize(), " | dados:", data)
 
 
-@sio.event
-def reset_simulation():
-    simu = simulation.SimulationController()
-    simu.reset()
-    print('Arquivos de simulação Resetados!')
 
-@sio.event
-def disconnect():
-    print('disconnected from server')
+    @sio.event
+    def simulation_loop():
+        if simulation_state.simulation_running:
+            print("Loop simulação já está rodando!")
+            return
+        while True:
+            simulation_state.simulation_running = True
+            while not fila.empty():
+                data = fila.get()
+                print('Irá iniciar simulação com os dados:', data)
+                with subprocess.Popen(f'./simulation.exe \"[{data["medicaoA"]}, {data["medicaoB"]}, {data["medicaoC"]}, {data["medicaoTemp"]}, \'{data["sensor"]}\']\"', stdin=subprocess.PIPE, stdout=subprocess.PIPE) as proc:
+                    print(proc.stdout.read())
+                # p = subprocess.run([sys.executable, "./simulation.exe", json.dumps([data["medicao"], data["sensor"]])],
+                #                    capture_output=True, text=True)
+                # print("LOG EXECUCAO SUBPROCESS:", p.stdout)
+        simulation_state.simulation_running = False
+
+    @sio.event
+    def send_img_loop():
+        if simulation_state.files_running:
+            print("Loop dos arquivos já está rodando!")
+            return
+        print("Iniciou o loop dos arquivos")
+        img_file_name = "img_send.json"
+        progress_file_name = "progress_simulation.txt"
+        while True:
+            simulation_state.files_running = True
+            time.sleep(3)
+            dir_list = os.listdir("./")
+            if img_file_name in dir_list:
+                try:
+                    with open(img_file_name) as file:
+                        sio.emit('request_update_image', json.loads(file.read()))
+                    os.remove(img_file_name)
+                except:
+                    print("Quebrou ao tentar abrir arquivo das imagens")
+
+            if progress_file_name in dir_list:
+                try:
+                    with open(progress_file_name) as file:
+                        sio.emit('progress', file.read())
+                except:
+                    print("Quebrou ao tentar abrir arquivo progress")
+        print("Finalizou Loop")
+        simulation_state.files_running = False
 
 
-sio.connect("https://server-sensor.fly.dev/", wait_timeout=20)
-sio.wait()
+    @sio.event
+    def reset_simulation():
+        simu = simulation.SimulationController()
+        simu.reset()
+        print('Arquivos de simulação Resetados!')
 
-#codigo do femm
+    @sio.event
+    def disconnect():
+        print('disconnected from server')
+
+
+    sio.connect("https://server-sensor.fly.dev/", wait_timeout=20)
+    sio.wait()
+except Exception as e:
+    print(e)
+    input()
