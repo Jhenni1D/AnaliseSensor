984f7784455a2f11c49a8f4ea5be3c8ea7dc8d07
Elielson Barbosa <elielsonbr.com@gmail.com>
2024-07-20 20:48:33 -0300

    refact: renomeando todas as rotas e traduzindo pro inglês

--- server/app.py
+++ server/app.py
@@ -1,11 +1,7 @@
-import random
-
 from flask import Flask, request, send_file, render_template, jsonify, redirect, url_for
 import os
 from app_service_server import *
-
 from flask_socketio import SocketIO
-import json
 from requests import get
 import time
 
@@ -19,161 +15,86 @@ def home():  # def = função
     return render_template("home.html")
 
 
-@app.route('/nova-simulacao')
+@app.route('/reset-simulation-data')
 def nova_simulacao():  # def = função
     io.emit("reset_simulation")
-    return "Arquivos de simulação resetados!"
+    return "Simulation data deleteds!"
 
 
-@app.route('/dados', methods=['POST'])
-def receber():
-    dado = request.json
-    date = pegar_data_formatada()
-    hour = pegar_hora_formatada()
-    dado_armazenar = dado | {"data": date,
+@app.route('/store/new-simulation', methods=['POST'])
+def store_and_init_new_simulation():
+    data = request.json
+    date = get_date_formatted()
+    hour = get_hour_formatted()
+    store_firebase_data = data | {"data": date,
                              "hora": hour, "data_hora": f"{date} {hour}"}
 
-    folder_incompleted = get_folder_incompleted()
+    folder_incompleted = get_folder_uncompleted()
     if len(folder_incompleted) == 0:
-        new_folder_name, new_folder = pegar_nova_pasta_formatada()
-        new_folder[new_folder_name]["Sensores"] = [dado_armazenar]
+        new_folder_name, new_folder = get_new_folder_info_template()
+        new_folder[new_folder_name]["Sensores"] = [store_firebase_data]
         print(f"nova pasta a ser registrada: {new_folder}")
         register_new_folder(new_folder)
-        dado["folder_name"] = new_folder_name
-        print(f"criou pasta - Armazenou o dado: {dado_armazenar}")
+        data["folder_name"] = new_folder_name
+        print(f"criou pasta - Armazenou o data: {store_firebase_data}")
     else:
         folder_name, folder_data = folder_incompleted[0]
-        folder_data[folder_name]["Sensores"].append(dado_armazenar)
-        print(f"Armazenou o dado: {dado_armazenar}")
+        folder_data[folder_name]["Sensores"].append(store_firebase_data)
+        print(f"Armazenou o data: {store_firebase_data}")
         register_new_folder(folder_data)
-        dado["folder_name"] = folder_name
+        data["folder_name"] = folder_name
 
-    print(f"dado enviado para a VM: {dado}")
-    io.emit("insert_queue", dado)
+    print(f"data enviado para a VM: {data}")
+    io.emit("insert_queue", data)
     return "deu tudo certo"
 
 
 # terceira rota de baixar os dados em formato de excel
-@app.route("/baixar_dado")
-def baixar_dado():
-    escrever_dados_arquivo_csv(pegar_todos_dados_bd())
+@app.route("/download-simulation-data")
+def download_simulation_data():
+    write_csv_data(get_all_sensors())
     path = os.getcwd() + "/output.zip"  # caminho do codigo atual
     return send_file(
         path, as_attachment=True
     )  # send_file do flask pegue o arquivo do direotiro pra poder baixar
 
 
-@app.route("/visualizar_dado")
-def visualizar_dado():
-    return jsonify(pegar_todos_dados_bd())
+@app.route("/show-data/.json")
+def show_data():
+    return jsonify(get_all_sensors())
+
+#TODO: ajustar rota e seu método
+@app.route("/last-sensor-data/<sensor>/.json")
+def last_sensor_data(sensor):
+    return jsonify(get_last_sensor_data(sensor))
 
 
-@app.route("/ultimo-dado-sensor/<sensor>")
-def ultimo_dado_sensor(sensor):
-    return jsonify(pegar_ultimo_dado_do_sensor(sensor))
-
-
-@app.route("/pastas")
-def visualizar_pastas():
-    pastas = []
-    with open("folders.json", "r") as file:
-        pastas = json.loads(file.read())
-    return jsonify(pastas)
-
-
-def check_and_get_img(link, folder_img, all_folders):
-    img_link = link if folder_img in all_folders else "https://cdn.dribbble.com/users/386433/screenshots/1689880/placehold.gif"
-    return img_link
-
-
-def get_image_data(pasta):
-    link_all_folders_imgs = "https://firebasestorage.googleapis.com/v0/b/simulacao-femm.appspot.com/o/"
-    all_folders = get(link_all_folders_imgs).json()['items']
-    all_folders = [data_img['name'] for data_img in all_folders]
-    link = f"https://firebasestorage.googleapis.com/v0/b/simulacao-femm.appspot.com/o/{pasta}%2F{'{}'}.png?alt=media"
-    data = [
-        {
-            "type": "M",
-            "name": "M0",
-            "img": ""
-        },
-        {
-            "type": "M",
-            "name": "M1",
-            "img": ""
-        },
-        {
-            "type": "M",
-            "name": "M2",
-            "img": ""
-        },
-        {
-            "type": "M",
-            "name": "M3",
-            "img": ""
-        },
-        {
-            "type": "T",
-            "name": "T0",
-            "img": ""
-        },
-        {
-            "type": "T",
-            "name": "T1",
-            "img": ""
-        },
-        {
-            "type": "T",
-            "name": "T2",
-            "img": ""
-        },
-        {
-            "type": "T",
-            "name": "T3",
-            "img": ""
-        },
-        {
-            "type": "TERMICO",
-            "name": "TERMICO",
-            "img": ""
-        }
-    ]
-    for d in data:
-        d["img"] = check_and_get_img(link.format(d["name"]), f'{pasta}/{d["name"]}.png', all_folders)
-    return data
-
-
-@app.route("/visualizar/<pasta>")
-def visualizar_pasta(pasta):
-    link_folder = f"https://firebasestorage.googleapis.com/v0/b/simulacao-femm.appspot.com/o/{pasta}%2FM0.png"
+@app.route("/uncompleted-simulation/dashboard/<folder>")
+def uncompleted_simulation_dashboard(folder):
+    link_folder = f"https://firebasestorage.googleapis.com/v0/b/simulacao-femm.appspot.com/o/{folder}%2FM0.png"
     folder_exist = get(link_folder).status_code.real != 404
 
     if folder_exist:
-        return render_template("medicoes3.html", data=get_image_data(pasta), folder_name=pasta)
-    return render_template("medicoes3.html", data=[], folder_name=pasta)
+        return render_template("medicoes3.html", data=get_image_data(folder), folder_name=folder)
+    return render_template("medicoes3.html", data=[], folder_name=folder)
 
 
-@app.route("/ultima-pasta")
-def ultima_pasta():
-    data = {"pasta": None}
-    folder_incompleted = get_folder_incompleted()
-    if len(folder_incompleted) != 0:
-        data["pasta"] = folder_incompleted[0][0]
+@app.route("/uncompleted-simulation/.json")
+def uncompleted_simulation():
+    data = {"folder": None}
+    folder_uncompleted = get_folder_uncompleted()
+    if len(folder_uncompleted) != 0:
+        data["folder"] = folder_uncompleted[0][0]
     return jsonify(data)
 
 
-@app.route("/dashboard/ultima-pasta")
-def dashboard_ultima_pasta():
-    folder_incompleted = get_folder_incompleted()
-    if len(folder_incompleted) == 0:
+@app.route("/to/uncompleted-simulation/dashboard")
+def uncompleted_simulation_dashboard():
+    folder_uncompleted = get_folder_uncompleted()
+    if len(folder_uncompleted) == 0:
         return redirect(url_for("/"))
-    print(folder_incompleted[0][0])
-    return redirect(location=f"/visualizar/{folder_incompleted[0][0]}")
-
-
-@app.route("/get_corrent_values")
-def get_corrent_values():
-    return jsonify(get_date_and_sensors_values_for_graph())
+    print(folder_uncompleted[0][0])
+    return redirect(location=f"/visualizar/{folder_uncompleted[0][0]}")
 
 
 @io.event
@@ -297,4 +218,4 @@ def corrent_data_updater():
 
 
 if __name__ == "__main__":
-    io.run(app, host="0.0.0.0", port=8080, allow_unsafe_werkzeug=True)
+    io.run(app, host="0.0.0.0", port=8080, allow_unsafe_werkzeug=True, debug=True)
