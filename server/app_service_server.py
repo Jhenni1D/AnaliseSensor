31f58a4f7d974a14b04e69fa61c7ab0c9e2fd7f9
Elielson Barbosa <elielsonbr.com@gmail.com>
2024-07-21 14:24:02 -0300

    refact: refaz lógica para buscar os dados que serão usados para plotar os gráficos

--- server/app_service_server.py
+++ server/app_service_server.py
@@ -104,37 +104,17 @@ def get_date_and_sensors_values_for_graph():
     if len(folders_data) == 0:
         return {}
     folder_name, folder_data = folders_data[0]
-    sensors_names = [fn for fn in folder_data[folder_name]["Sensores"] if fn in ['SensorA', 'SensorB', 'SensorC']]
-    list_unpacked_unique_values = lambda list_filter: list(
-        {a for b in [{y for y in x} for x in list_filter] for a in b})
-    list_unpacked = lambda list_filter: [a for b in [[y for y in x] for x in list_filter] for a in b]
-    list_unpacked_hashed = lambda list_filter: {list(x.keys())[0]: x[list(x.keys())[0]] for x in list_filter}
-
-    sensors = folder_data[folder_name]['Sensores']
-
-    def del_sensor_not_need(sensor_name):
-        if sensor_name in sensors:
-            del sensors[sensor_name]
-
-    del_sensor_not_need("SensorRPM")
-    del_sensor_not_need("SensorTemp")
-    del_sensor_not_need("SensorTensao")
-    all_values = [
-        [{f"{sensor_info['data']} {sensor_info['hora']}": {"sensor": sensor, "value": sensor_info['medicao']}} for
-         sensor_info in sensors[sensor]] for sensor in sensors]
-    all_values = list_unpacked(all_values)
-    all_values_hashed = list_unpacked_hashed(all_values)
-    all_dates = [[f"{sensor_info['data']} {sensor_info['hora']}" for sensor_info in sensors[sensor]] for sensor in
-                 sensors]
-    all_dates = list_unpacked_unique_values(all_dates)
-    all_dates.sort()
-
-    sensor_data = lambda sensor_name: [all_values_hashed[d]['value'] for d in all_dates if
-                                       sensor_name in all_values_hashed[d]['sensor']]
-
-    graph_data = {sensor_name: sensor_data(sensor_name) for sensor_name in sensors_names}
-    graph_data["dates"] = all_dates
-    return graph_data
+    sorted_sensors = sorted(folder_data[folder_name]['Sensores'], key=lambda d: d['data_hora'])
+    data_graph = {}
+    keys = list(sorted_sensors[0].keys())
+    for data in sorted_sensors:
+        for key in keys:
+            if key not in data_graph:
+                data_graph[key] = []
+            data_graph[key].append(data[key])
+    del data_graph['data']
+    del data_graph['hora']
+    return data_graph
 
 
 def check_and_get_img(link, folder_img, all_folders):
