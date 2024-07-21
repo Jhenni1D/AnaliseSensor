44a85412e8691014f92385a15fb1bf0711e6100e
Elielson Barbosa <elielsonbr.com@gmail.com>
2024-07-21 14:27:30 -0300

    feat: add slider_config.js para controle das datas nos gráficos

--- server/static/js/simulation_dashboard.js
+++ server/static/js/simulation_dashboard.js
@@ -1,9 +1,5 @@
 $(document).ready(function () {
-  let progresso = document.querySelector("#progresso");
-  let namespace = '';
-  let host = location.protocol + '//' + document.domain + ':' + location.port + namespace;
-
-  let eficience_percent = document.querySelector("#eficiencia");
+  let progress_simulation_element = document.querySelector("#progress");
 
   let m_images = {
     "M0": document.querySelector("#m0"),
@@ -19,20 +15,28 @@ $(document).ready(function () {
     "T3": document.querySelector("#t3")
   }
 
-  let grafico_corrent = document.querySelector("#corrente");
-
-    let corrent_chart = new Chart(document.querySelector("#corrente"),
+  let corrent_chart = new Chart(document.querySelector("#corrente"),
     {
-        type: 'line',
-        data: [],
-        options:
-        {
-            scales: {
-            }
+      type: 'line',
+      data: [],
+      options:
+      {
+        scales: {
+          x: {
+            min: minSliderValue,
+            max: maxSliderValue
+          }
         }
+      }
     });
 
-  let graficos_images = {
+  slider.noUiSlider.on('update', function (value) {
+    corrent_chart.options.scales.x.min = parseInt(value[0]);
+    corrent_chart.options.scales.x.max = parseInt(value[1]);
+    corrent_chart.update();
+  });
+
+  let elements_graph = {
     "SensorTensao": document.querySelector("#tensao"),
     "SensorTemp": document.querySelector("#temperatura"),
     "SensorRPM": document.querySelector("#rpm"),
@@ -40,230 +44,148 @@ $(document).ready(function () {
   }
 
   let charts_graphs = {
-    "SensorTensao": CreateChart(graficos_images["SensorTensao"]),
-    "SensorTemp": CreateChart(graficos_images["SensorTemp"]),
-    "SensorRPM": CreateChart(graficos_images["SensorRPM"]),
-    "eficiencia": CreateChart(graficos_images["eficiencia"])
+    "SensorTensao": CreateChart(elements_graph["SensorTensao"]),
+    "SensorTemp": CreateChart(elements_graph["SensorTemp"]),
+    "SensorRPM": CreateChart(elements_graph["SensorRPM"]),
+    "eficiencia": CreateChart(elements_graph["eficiencia"])
   }
 
   let modal_img = document.querySelector("#modal_img")
   let modal_title = document.querySelector("#title_modal")
 
-  console.log(dado)
-  console.log(host)
-
   UpdateImages();
   RegisterActions();
   PlotGraphData();
 
-  function CreateChart(canvas_element)
-  {
+  function CreateChart(canvas_element) {
     return new Chart(canvas_element,
-                {
-                    type: 'line',
-                    data: [],
-                    options:
-                    {
-                        scales: {
-                        }
-                    }
-                });
+      {
+        type: 'line',
+        data: [],
+        options:
+        {
+          scales: {
+          }
+        }
+      });
   }
 
-  function UpdateImages()
-  {
-      if (dado.length !== 0) {
-        for (d of dado) {
-          if (d["name"] in m_images) {
-            m_images[d["name"]].src = d["img"];
-            m_images[d["name"]].classList.remove('placeholder')
-          }
+  function UpdateImages() {
+    if (dado.length !== 0) {
+      for (d of dado) {
+        if (d["name"] in m_images) {
+          m_images[d["name"]].src = d["img"];
+          m_images[d["name"]].classList.remove('placeholder')
+        }
 
-           if (d["name"] in t_images) {
-             t_images[d["name"]].src = d["img"];
-             t_images[d["name"]].classList.remove('placeholder')
-           }
+        if (d["name"] in t_images) {
+          t_images[d["name"]].src = d["img"];
+          t_images[d["name"]].classList.remove('placeholder')
         }
       }
+    }
   }
 
-  function RegisterActions()
-  {
-      for (img in m_images)
-      {
-        m_images[img].addEventListener('click', ShowModalImage);
-      }
+  function RegisterActions() {
+    for (img in m_images) {
+      m_images[img].addEventListener('click', ShowModalImage);
+    }
 
-      for (img in t_images)
-      {
-        t_images[img].addEventListener('click', ShowModalImage);
-      }
+    for (img in t_images) {
+      t_images[img].addEventListener('click', ShowModalImage);
+    }
   }
 
-  function ShowModalImage(event)
-  {
+  function ShowModalImage(event) {
     modal_img.src = event.srcElement.src;
     modal_title.innerHTML = event.srcElement.id.toUpperCase();
   }
 
-  function PlotGraphData()
-  {
-    if(graph_data == undefined)
-    {
-        return;
+  function PlotGraphData() {
+    if (graph_data == undefined) {
+      return;
     }
 
     let colors = ['blue', 'red', 'green', 'purple', 'orange']
     let index_color = 0;
-    let corrent_graph_data =
-    {
-        datasets:
-        [
-        ]
-    }
-    try
-    {
-        for(graph in graph_data)
-        {
-            if(graph_data[graph].hasOwnProperty("values") && graficos_images.hasOwnProperty(graph))
-            {
-                let ctx = graficos_images[graph];
-                let data =
-                {
-                    datasets:
-                    [
-                        {
-                            borderColor: colors[index_color++],
-                            label: `${graph.toLocaleUpperCase()} / Tempo`,
-                            data: graph_data[graph]['values']
-                        }
-                    ]
-                }
+    try {
+      for (graph in graph_data) {
 
-                charts_graphs[graph].data = data;
-                charts_graphs[graph].update('resize');
-            }
+        if (graph.includes("Sensor") === false || graph.includes("SensorA") || graph.includes("SensorB") || graph.includes("SensorC")) {
+          continue;
         }
+
+        if (charts_graphs[graph].data.length !== 0) {
+          charts_graphs[graph].update();
+          continue;
+        }
+
+        let data =
+        {
+          datasets:
+            [
+              {
+                borderColor: colors[index_color++],
+                label: `${graph.toLocaleUpperCase()} / Tempo`,
+                data: graph_data[graph]
+              }
+            ],
+          labels: graph_data["data_hora"]
+        }
+
+        if (charts_graphs[graph].data.length === 0) {
+          charts_graphs[graph].data = data;
+        }
+        charts_graphs[graph].update();
+      }
     }
-    catch
-    {
-        console.log("Erro ao criar os gráficos gerais");
+    catch (err) {
+      console.log("Erro ao criar os gráficos gerais");
+      console.log(err);
     }
   }
 
-  function PlotCorrentGraph()
-  {
+  function PlotCorrentGraph() {
 
-    if(corrent_graph_data_server == null)
-    {
-        return;
+    if (graph_data == null) {
+      return;
+    }
+
+    if (corrent_chart.data.length !== 0) {
+      corrent_chart.options.scales.x.min = currentMinSliderValue;
+      corrent_chart.options.scales.x.max = currentMaxSliderValue;
+      corrent_chart.update();
+      return;
     }
 
     let corrent_colors = ['blue', 'red', 'green']
-    let index_color = 0;
     let index_corrent_color = 0;
     let corrent_graph_data =
     {
-        datasets:
+      datasets:
         [
-            {
-                    borderColor: corrent_colors[index_corrent_color++],
-                    label: "SensorA / Tempo",
-                    data: corrent_graph_data_server["SensorA"]
-            },
-            {
-                    borderColor: corrent_colors[index_corrent_color++],
-                    label: "SensorB / Tempo",
-                    data: corrent_graph_data_server["SensorB"]
-            },
-            {
-                    borderColor: corrent_colors[index_corrent_color++],
-                    label: "SensorC / Tempo",
-                    data: corrent_graph_data_server["SensorC"]
-            }
+          {
+            borderColor: corrent_colors[index_corrent_color++],
+            label: "SensorA / Tempo",
+            data: graph_data["SensorA"]
+          },
+          {
+            borderColor: corrent_colors[index_corrent_color++],
+            label: "SensorB / Tempo",
+            data: graph_data["SensorB"]
+          },
+          {
+            borderColor: corrent_colors[index_corrent_color++],
+            label: "SensorC / Tempo",
+            data: graph_data["SensorC"]
+          }
         ],
-        labels: corrent_graph_data_server["dates"]
+      labels: graph_data["data_hora"]
     }
-    if(corrent_chart.data.length === 0)
-    {
-        corrent_chart.data = corrent_graph_data;
-    }
+    corrent_chart.data = corrent_graph_data;
     corrent_chart.update();
   }
 
-  function IsCorrentSensor(sensor)
-  {
-    return sensor == "SensorA" || sensor == "SensorB" || sensor == "SensorC";
-  }
-   // ATUALIZAÇÃO DOS GRÁFICOS
-    function FormatSensorsData(sensors_data)
-    {
-        sensors_data_formatted = {}
-        for(sensor in sensors_data)
-        {
-            if(sensor == "SensorA" || sensor == "SensorB" || sensor == "SensorC")
-            {
-                continue;
-            }
-            sensors_data_formatted[sensor] = {"values": []}
-            for(sensor_info of sensors_data[sensor])
-            {
-                graph_info = {"x": sensor_info["data"]+" "+sensor_info["hora"], "y": parseFloat(sensor_info["medicao"])}
-                sensors_data_formatted[sensor]["values"].push(graph_info)
-            }
-        }
-
-        return sensors_data_formatted;
-    }
-
-    function FormatCorrentSensorData(corrent_sensor_data)
-    {
-        let only_values = []
-        for (data of corrent_sensor_data)
-        {
-            only_values.push(data["y"])
-        }
-
-        return only_values;
-    }
-
-    function updateGraphs(handlerResult, link)
-    {
-      let xhttp = new XMLHttpRequest();
-      xhttp.onload = handlerResult;
-      xhttp.open("GET", link, true);
-      xhttp.send();
-    }
-
-    function updateSingularGraphs()
-    {
-      handlerResult = function()
-      {
-        var sensors_data = JSON.parse(this.responseText);
-        graph_data = FormatSensorsData(sensors_data);
-        setTimeout(PlotGraphData, 1000);
-        setTimeout(updateSingularGraphs, 2000);
-      }
-
-      updateGraphs(handlerResult, `https://simulacao-femm-default-rtdb.firebaseio.com/medicoes/Pastas/${folder_name}/Sensores/.json`);
-    }
-
-    function updateCorrentGraphs()
-    {
-        handlerResult = function()
-      {
-        corrent_graph_data_server = JSON.parse(this.responseText);
-        setTimeout(PlotCorrentGraph, 1000);
-        setTimeout(updateCorrentGraphs, 2000);
-      }
-
-      updateGraphs(handlerResult, `http://localhost:8080/get_corrent_values`);
-    }
-
-    updateSingularGraphs();
-    //updateCorrentGraphs();
-
-    // SOCKETIO LOGIC
 
   let socket = io();
 
@@ -281,27 +203,33 @@ $(document).ready(function () {
 
 
   socket.on('progress_value', function (prog_value) {
-    progresso.style["width"] = `${prog_value}%`;
-    progresso.innerHTML = `${prog_value}%`;
+    progress_simulation_element.style["width"] = `${prog_value}%`;
+    progress_simulation_element.innerHTML = `${prog_value}%`;
   });
 
 
-  socket.on('corrent_data_updater', function(data){
-
-    if(corrent_graph_data_server === null)
-    {
-        corrent_graph_data_server = data;
-        PlotCorrentGraph();
-        return;
+  socket.on('corrent_data_updater', function (data) {
+    graph_data_cache = data;
+    if (graph_data === null) {
+      graph_data = data;
+      PlotCorrentGraph();
+      PlotGraphData();
+      maxSliderValue = data['data_hora'].length - 1;
+      UpdateSlider();
+      return;
     }
-    let need_update = corrent_graph_data_server['dates'].slice(-1)[0] !== data['dates'].slice(-1)[0]
-    if(need_update)
-    {
-        for(key in data)
-        {
-            corrent_graph_data_server[key].push(data[key].slice(-1)[0]);
-        }
-        PlotCorrentGraph();
+
+    let need_update = graph_data['data_hora'].slice(-1)[0] !== data['data_hora'].slice(-1)[0]
+    //console.log(graph_data['data_hora']);
+
+    if (need_update) {
+      for (key in data) {
+        graph_data[key].push(data[key].slice(-1)[0]);
+      }
+      PlotCorrentGraph();
+      PlotGraphData();
+      maxSliderValue = graph_data_cache['data_hora'].length - 1;
+      UpdateSlider();
     }
   });
 
---
+++ server/static/js/slider_config.js
@@ -0,0 +1,48 @@
+const rangeSlider_min = 0;
+const rangeSlider_max = 100;
+var minSliderValue = 0;
+var maxSliderValue = 100;
+var currentMinSliderValue = 0;
+var currentMaxSliderValue = 100;
+
+var slider = document.getElementById('slider');
+var config = {
+	start: [minSliderValue, maxSliderValue],
+	connect: true,
+	range: {
+		'min': minSliderValue,
+		'max': maxSliderValue
+	},
+	tooltips: [
+		{
+			to: function (value) {
+				if (graph_data_cache === null) {
+					return value;
+				}
+				return graph_data_cache['data_hora'][parseInt(value)];
+			}
+		},
+		{
+			to: function (value) {
+				if (graph_data_cache === null) {
+					return value;
+				}
+				console.log(parseInt(value))
+				return graph_data_cache['data_hora'][parseInt(value)];
+			}
+		}
+	]
+}
+
+noUiSlider.create(slider, config);
+
+slider.noUiSlider.on('update', function (value) {
+	currentMinSliderValue = parseInt(value[0]);
+	currentMaxSliderValue = parseInt(value[1]);
+});
+
+function UpdateSlider() {
+	config.range['max'] = maxSliderValue;
+	config.start[1] = currentMaxSliderValue;
+	slider.noUiSlider.updateOptions(config);
+}
