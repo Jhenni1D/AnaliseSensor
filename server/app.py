5d30a884d116ab278478c8fdfd6a9c302a8cd356
Elielson Barbosa <elielsonbr.com@gmail.com>
2024-07-22 01:25:55 -0300

    feat: ativando opção de downlaod dos dados dos sensores

--- server/app.py
+++ server/app.py
@@ -80,14 +80,13 @@ def to_uncompleted_simulation_dashboard():
     folder_uncompleted = get_folder_uncompleted()
     if len(folder_uncompleted) == 0:
         return redirect(url_for("/"))
-    print(folder_uncompleted[0][0])
     return redirect(location=f"/uncompleted-simulation/dashboard/{folder_uncompleted[0][0]}")
 
 
+@app.route('/download/<filename>', methods=['GET'])
 @app.route('/download/<filename>/', methods=['GET'])
 def download(filename):
     get_excel_of_folder(filename)
-    print(filename)
     filename = filename + "_Data.xls" if ".xls" not in filename else filename
     return send_file(get_bytes_file(filename), mimetype='application/xls', as_attachment=True,
                      download_name=filename)
